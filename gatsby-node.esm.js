b5559c44cc786de0da5bbb92e55379957ffc2306
Bennett Lin <bennettslin@gmail.com>
2021-09-12 13:57:07 -0700

    Minor changes.

--- gatsby-node.esm.js
+++ gatsby-node.esm.js
@@ -9,12 +9,3 @@ export const createPages = ({ actions }) => {
         })
     })
 }
-
-// export const onCreateWebpackConfig = ({ actions }) => {
-//     actions.setWebpackConfig({
-//         resolve: {
-//             // Import from files without specifying extensions.
-//             extensions: ['.js', '.jsx', '.scss'],
-//         },
-//     })
-// }
